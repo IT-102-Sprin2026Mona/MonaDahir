d26dff002ef34e513fd33b5e56be9d85ab391c9e
IT-102-Sprin2026Mona <dahir.hodan.2@student.greenriver.edu>
2026-06-19 20:16:33 +0000

    Created a read script

--- end/CH04/ReadFile.py
+++ end/CH04/ReadFile.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python3
 # Sample script that reads from a file
-# By Ed Goad
-# 12/12
+# By Mona
+"""
+This is to read the file i created from the script writefile.py
+"""
 
-import os
 
-# Get current file directory
-script_path = os.path.abspath( __file__ )
-script_dir = os.path.dirname( script_path )
-# Build file path
-file_path = os.path.join(script_dir, "ips.txt")
-
-# Open file for reading
-ip_file = open(file_path, "r")
-
-# Read the contents of the file and print to screen
-ip_addresses = ip_file.read()
-print(ip_addresses)
-
-# Close the file
-ip_file.close()
+#Create a loop to open file and read its contents
+with open("hackme.txt", "r") as file 
+    contents = file.read()
+print("hete is someone to hack - info dump")
+print(contents)
 
