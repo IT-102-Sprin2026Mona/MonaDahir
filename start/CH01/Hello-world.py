64d042bfa5f71d7c8ccfb00ce49686257f237a1b
IT-102-Sprin2026Mona <dahir.hodan.2@student.greenriver.edu>
2026-06-20 04:25:19 +0000

    Hello world script

--- start/CH01/Hello-world.py
+++ start/CH01/Hello-world.py
@@ -1,5 +1,13 @@
 '''
-Creat a condintional that takes a inpyt and if its a yes then print a reply
+This is just a script to prompt user for user name, 
+then print hello and a positive affirmation
 '''
 
-#
+#Prompt the user user for their name
+guest_name = input("What is your name: ")
+
+#This is to say hello
+print("Hello, ", guest_name)
+
+#Print positive affirimation
+print("Today is going to be a great day. ")
